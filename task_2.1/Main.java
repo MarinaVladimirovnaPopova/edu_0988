5f8e2cc86370c64e0bd7da1b89d3131485d07be8
MarinaVladimirovnaPopova <88132239+MarinaVladimirovnaPopova@users.noreply.github.com>
2021-07-30 11:27:27 +0300

    Create Main.java

---
+++ task_2.1/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        String[] mama = {"Мама", "Мыла", "Раму"};
+        for(int i=0; i<=2; i++){
+            System.out.println(mama[i]);
+        }
+
+    }
+}
