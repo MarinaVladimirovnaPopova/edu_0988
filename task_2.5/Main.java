724057b32d9c8c5878c8df07ee5d9e6610f0394b
MarinaVladimirovnaPopova <88132239+MarinaVladimirovnaPopova@users.noreply.github.com>
2021-07-31 01:14:47 +0300

    Create Main.java

---
+++ task_2.5/Main.java
@@ -0,0 +1,19 @@
+/*
+Задание: Запросить у пользователя ввод числа и сохранить это число в переменную a.
+Если переменная a равна 10, то выведите 'Верно', иначе выведите 'Неверно'.
+*/
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        System.out.print( "веедите целое число: " );
+        int a = scan.nextInt();
+            if ( a == 10 ){
+                System.out.print( "Верно");
+                }
+            else {
+                System.out.print( "Неверно");
+            }
+
+    }
+}
