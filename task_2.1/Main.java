bd245614c41aed61f81d584812c567a53ccb2542
MarinaVladimirovnaPopova <88132239+MarinaVladimirovnaPopova@users.noreply.github.com>
2021-07-30 12:21:35 +0300

    Update Main.java

--- task_2.1/Main.java
+++ task_2.1/Main.java
@@ -1,12 +1,16 @@
-ppublic class Main {
+public class Main {
     public static void main(String[] args) {
         String[] mama = {"Мама", "Мыла", "Раму"};
         for(int i=0; i<=2; i++){
             for(int j=0; i<=2; i++){
                 for(int k=0; i<=2; i++){
                     String word = mama[i]+mama[j]+mama[k];
+                    if ([i] != [j], [i] != [k], [j] != [k]);
                     System.out.println(""+word);
                 }
             }
 
         }
+
+    }
+}
