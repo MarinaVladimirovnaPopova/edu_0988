6a581c22ad05f5906623b4059fda803fb0e258af
MarinaVladimirovnaPopova <88132239+MarinaVladimirovnaPopova@users.noreply.github.com>
2021-07-31 01:00:47 +0300

    Update Main.java

--- task_2.4/Main.java
+++ task_2.4/Main.java
@@ -7,10 +7,10 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
-        System.out.print( "веедите размер вклада: " ); //напечатать строку с просьбой суммы
-        int sum = scan.nextInt();//пользователь вводит сумму вклада и присвоить ему переменную
-        System.out.print( "веедите размер процентов по вкладу: " );//напечатать строку с просьбой указать процент
-        int prs = scan.nextInt();//вести процент и присвоить ему переменную
+        System.out.print( "веедите размер вклада: " ); 
+        int sum = scan.nextInt();
+        System.out.print( "веедите размер процентов по вкладу: " );
+        int prs = scan.nextInt();
                 for (int i = 1; i <= 5; ++i){
                     System.out.print( i + " год = ");
                     System.out.println (sum + sum*prs*0.01);
