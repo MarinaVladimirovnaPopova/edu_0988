719f2bf6c2741a4312aed4e5f4581a95228e4b93
MarinaVladimirovnaPopova <88132239+MarinaVladimirovnaPopova@users.noreply.github.com>
2021-08-05 23:53:44 +0300

    Create Main.java

---
+++ task_4.5/Main.java
@@ -0,0 +1,17 @@
+/*
+task_4.5
+Создать абстрактный класс Animal с характеристиками животного.
+Создать класс Horse который наследуется от Animal, в классе Horse реализовать метод public void run(){ System.out.println("Игого, я поскакал(а)"); }
+Создать класс Pegasus который наследуется от Horse, в классе Pegasus реализовать метод public void fly(){ System.out.println("Игого, я полетел(а)"); }
+Создать объект лошади и вызвать метод run();
+Создать объект пегаса и вызвать метод fly();
+*/
+
+public class Main{
+    public static void main(String[] args) {
+        Horse horse = new Horse();
+        horse.run();
+        Pegasus pegasus = new Pegasus();
+        pegasus.fly();
+    }
+}
