47cc2fb9bd61f96165f144d29e220cd057f0dc61
MarinaVladimirovnaPopova <88132239+MarinaVladimirovnaPopova@users.noreply.github.com>
2021-08-05 23:55:32 +0300

    Create Animal.java

---
+++ task_4.5/Animal.java
@@ -0,0 +1,9 @@
+public abstract class Animal{
+    public static void main(String[] args) {
+
+        String nickname;
+        int age;
+        boolean  flying; //способность летать
+        boolean waterfowl; // способность плавать
+    }
+}
