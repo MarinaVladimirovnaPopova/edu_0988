d047bd600b4489d6d79cfb65c63650b5c2953880
MarinaVladimirovnaPopova <88132239+MarinaVladimirovnaPopova@users.noreply.github.com>
2021-08-10 16:39:17 +0300

    Update Main.java
    решена

--- task_5.4/Main.java
+++ task_5.4/Main.java
@@ -1,6 +1,7 @@
-import java.io.*;
-import java.util.Scanner;
+import java.io.FileInputStream;
+import java.io.IOException;
 import java.util.ArrayList;
+import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) throws IOException {
@@ -9,21 +10,38 @@ public class Main {
         System.out.print("Веедите путь к файлу: ");
         String pathFile = scanner.nextLine();
 
-    /*FileReader reader = new FileReader("pathFile");
-    int c;
-        while ((c = reader.read())!=-1){
-        System.out.print((char)c);*/
-
         FileInputStream fin = new FileInputStream(pathFile);
 
-        ArrayList<Character> nums = new ArrayList<>();
-        ArrayList<Character> nums2 = new ArrayList<>();
-        int  i = -1;
+        ArrayList<Integer> nums = new ArrayList<>();
+
+        int i=0;
+        String result = "";
         while ((i = fin.read()) != -1) {
-            char j = (char)i;
-            System.out.print(j); // верно выводит
-            nums.add(j);
+            if (i == 13) continue;// возврат каретки, пропускаем
+            if (i == 10) {// перенос строки
+                String[] NumArray = result.split(" ");//Разделяем
+                int n = Integer.parseInt(NumArray[0]);//Сохраняем
+                nums.add(n);
+                result = "";
+            } else {
+                result += (char) i;//Сохраняем в строку любые другие смволы прочитанные из файла
+            }
         }
-        System.out.print(nums); //запятые выводит (
+        fin.close();
+
+        for(int k = nums.size()-1 ; k > 0 ; k--){  // или использовать метод sort: Collections.sort(nums);
+            for(int j = 0 ; j < k ; j++){
+                if( nums.get(j) > nums.get(j+1)){
+                    int tmp =  nums.get(j);
+                    nums.set(j, nums.get(j+1));
+                    nums.set(j+1, tmp);
+                }
+            }
+        }
+        System.out.println("Итог: ");
+        for (int l = 0; l < nums.size(); l++) {
+
+            if (nums.get(l) % 2 == 0) System.out.println(nums.get(l));//Выводим только четные, отсортированные по возрастанию
+        }
     }
 }
