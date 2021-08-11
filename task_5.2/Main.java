bba9ce843ac2533e4b9d78c4d357d95ae295bb92
MarinaVladimirovnaPopova <88132239+MarinaVladimirovnaPopova@users.noreply.github.com>
2021-08-12 00:52:16 +0300

    Update Main.java

--- task_5.2/Main.java
+++ task_5.2/Main.java
@@ -36,3 +36,42 @@ public class Main {
 
         }
     }
+
+/*=========ВТОРОЙ ВАРИАНТ=========
+
+import java.util.Scanner;
+import java.util.Map;
+import java.util.HashMap;
+
+public class Main {
+    public static void main(String[] args) {
+        Map<String, String> persons = new HashMap<String, String>();
+        Scanner scanner = new Scanner(System.in);
+
+        while (true) {
+            System.out.println("Введите город проживания: ");
+            String city = scanner.nextLine();
+            if (city.isEmpty()) {
+                break;
+            }
+            System.out.println("Введите фамилию: ");
+            String family = scanner.nextLine();
+            if (family.isEmpty()) {
+                break;
+            }
+            persons.put(city, family);
+
+        }
+
+            System.out.println("Ввод данных завершен.\nВведите название интересующего города: ");
+            String askCity = scanner.nextLine();
+
+            if (persons.get(askCity)!=null) {
+                System.out.println(persons.get(askCity));
+            }
+            else System.out.println("Такого города нет в списке или вы ввели некорректное название.");
+
+        }
+    }
+
+*/
