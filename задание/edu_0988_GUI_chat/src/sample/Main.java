3afe4e580aa234488f4f0533ce48d2b769ffae30
MarinaVladimirovnaPopova <88132239+MarinaVladimirovnaPopova@users.noreply.github.com>
2021-08-12 11:47:02 +0300

    Create Main.java

---
+++ задание/edu_0988_GUI_chat/src/sample/Main.java
@@ -0,0 +1,28 @@
+package com.example.gui_caht_0988;
+
+import javafx.application.Application;
+import javafx.application.Platform;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class HelloApplication extends Application {
+
+    @Override
+    public void start(Stage primaryStage) throws Exception{
+        Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
+        primaryStage.setTitle("Hello World");
+        primaryStage.setScene(new Scene(root, 275, 300)); // Ширина, высота
+        primaryStage.show();
+        primaryStage.setOnCloseRequest(e->{
+            Platform.exit();
+            System.exit(0);
+        });
+    }
+
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
